c4129ab77c3c11964dce3821dc1ab79650a28b73
Revati Saraf <sarafrevati1321@gmail.com>
2026-05-12 13:50:08 +0530

    Update CORS for production

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://ai-email-reply-nine.vercel.app"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
